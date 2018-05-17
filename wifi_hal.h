bc04b72fe99174eb134bb450ee066a1c78bfd65f
Jackey Huang <chuang@quantenna.com>
2018-05-17 05:23:05 -0700

    ARRISXB6-7529 ARRISXB6-7496 TCXB6-4203 TCXB6-4171 implement set/get country code
    Reason for change: implement set/get country code without EP ready
    Test Procedure: check ARRISXB6-7529/ARRISXB6-7496/TCXB6-4203/TCXB6-4171 for verification steps.
    Risks: Low
    
    Change-Id: I9efea9fcc0000749c96ac8c53c45e9cf51c5e752
    Signed-off-by: Jackey Huang <chuang@quantenna.com>

--- wifi_hal.h
+++ wifi_hal.h
@@ -1019,7 +1019,7 @@ INT wifi_getRadioCountryCode(INT radioIndex, CHAR *output_string);
 *
 */
 INT wifi_setRadioCountryCode(INT radioIndex, CHAR *CountryCode);       
-
+INT wifi_pushRadioCountryCode(INT radioIndex, CHAR *CountryCode);
 
 /* wifi_pushCountryCode() function */
 /**
