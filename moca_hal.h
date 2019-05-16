c5940a54f162a1abf1089c027cdba1786b63379a
Thanh Nguyen <Thanh.Nguyen@technicolor.com>
2019-05-16 11:56:41 +0530

    CISCOXB3-5075,CISCOXB3-5651 : MoCA Mesh table support
    Reason for change: MoCA mesh entry to be displayed to user
    Test Procedure: Test with snmp command
    Risks: Low
    Signed-off-by: Soundararajan Kuppusamy<soundararajan.kuppusamy@technicolor.com>
    
    Change-Id: I86d6377610a9258c81d0802fcb1415e76246ed08
    (cherry picked from commit b81c4bb60502608b716056593a60d2f46fdebcd4)
    (cherry picked from commit 2f5cb81492f42a4ce971be4fbc53535f2caf6f19)

--- moca_hal.h
+++ moca_hal.h
@@ -404,6 +404,10 @@ typedef struct {
     by 'mocaMeshRxNodeIndex'.*/
     ULONG                           TxRate;
 
+    //Only for MoCA 2.x
+    ULONG                           TxRateNper;
+    ULONG                           TxRateVlper;
+
 } moca_mesh_table_t;
 #endif
 
